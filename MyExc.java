ad03013d36c18d1d2d115872f83088483d3c4577
AlexandrNikolaev0 <roketka1@gmail.com>
2019-02-15 10:57:54 +0300

    Delete MyExc.java

--- MyExc.java
+++
@@ -1,7 +0,0 @@
-public class MyExc extends Exception {
-    MyExc(BadBoy b){
-        if(b.mood == Mood.HAPPY){
-            System.out.println("Драки не будет, так как у Воланда хорошее настроение!");
-        }
-    }
-}
